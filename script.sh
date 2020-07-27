d7fa05c08012413f7b59c74c9efec3e0cd14eeaf
Artashes Bayazetyan <bayazetyan@gmail.com>
2020-07-27 14:54:28 +0400

    Update sdk and delete webrtc framework

--- script.sh
+++ script.sh
@@ -1,5 +1,3 @@
 rm -rf $PWD/ios/Pods/JitsiMeetSDK/Frameworks/JitsiMeet.framework
-rm -rf $PWD/ios/Pods/JitsiMeetSDK/Frameworks/WebRTC.framework
 
 cp -R $PWD/node_modules/rn-jitsi-meet/JitsiMeet.framework  $PWD/ios/Pods/JitsiMeetSDK/Frameworks
-cp -R $PWD/node_modules/rn-jitsi-meet/WebRTC.framework  $PWD/ios/Pods/JitsiMeetSDK/Frameworks
